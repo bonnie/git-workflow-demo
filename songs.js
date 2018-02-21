26548641cf6c18f6a83eabd9bbd604b7548165d1
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2018-02-20 20:51:33 -0800

    Add 'wheels on the bus'

---
+++ songs.js
@@ -0,0 +1,14 @@
+const wheelsOnTheBus = () => {
+  const thingsOnTheBus = { 
+    wheels: 'round and round',
+    wipers: 'swish swish swish',
+    people: 'up and down',
+  }
+
+  for (let thing in thingsOnTheBus) {
+    for (let i=0; i < 3; i++) {
+      console.log(`the ${thing} on the bus go ${thingsOnTheBus[thing]}`)
+    }
+    console.log('all through the town\n')
+  }
+}
